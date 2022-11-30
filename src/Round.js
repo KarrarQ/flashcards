defee1980a7bba487580aac41e66c23dc27121cf
Karrar Qasim <Karawr1996@gmail.com>
2022-11-29 17:10:37 -0700

    Finished up round tests and code

--- src/Round.js
+++ src/Round.js
@@ -1,22 +1,41 @@
+var Turn = require('./Turn');
+
 class Round {
-    constructor() {
-        this.currentCard = deck.cards[0];
+  constructor(deck) {
+    this.deck = deck;
+    this.turns = 0;
+    this.currentCard = deck.cards[0];
+    this.incorrectGuesses = [];
+  }
+
+  returnCurrentCard() {
+    return this.deck.cards[this.turns];
+  }
+
+  takeTurn(guess) {
+    let newTurn = new Turn(guess, this.currentCard);
+    newTurn.evaluateGuess();
+    if (guess !== this.currentCard.correctAnswer) {
+      this.incorrectGuesses.push(this.currentCard.id);
     }
-  
-    returnCurrentCard() {
-        this.currentCard = this.deck.cards[this.turns];
-        return this.deck.cards[this.turns];
-      }
-    
-      takeTurn(guess) {
-        let newTurn = new Turn(guess, this.currentCard);
-        this.turns++;
-        this.returnCurrentCard();
-        if (guess !== this.currentCard.correctAnswer) {
-            this.incorrectGuesses.push(this.currentCard.id)
-          }
-        // return newTurn;
-      }
+    this.turns++;
+    this.currentCard = this.returnCurrentCard();
+    return newTurn.giveFeedback();
+  }
+
+  calculatePercentCorrect() {
+    if (this.turns === this.incorrectGuesses.length) {
+      return `0%`;
+    } else {
+      return `${((this.turns - this.incorrectGuesses.length) / this.turns) * 100 }%`;
+    }
+  }
+
+  endRound() {
+    let result = `** Round over! ** You answered ${this.calculatePercentCorrect()} of the questions correctly!`;
+    console.log(result);
+    return result;
+  }
 }
 
 module.exports = Round;
